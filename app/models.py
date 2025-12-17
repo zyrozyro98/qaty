0b9c3db4bf674ef8309ed30ac9d8b94abe5d8879
zyrozyro98 <zyrozyro98@gmail.com>
2025-12-17 09:33:14 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -89,3 +89,19 @@ class Market(db.Model):
     
     def __repr__(self):
         return f'<Market {self.name}>'
+
+class Notification(db.Model):
+    __tablename__ = 'notifications'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    is_read = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    user = db.relationship('User', foreign_keys=[user_id])
+    
+    def __repr__(self):
+        return f'<Notification {self.title}>'
